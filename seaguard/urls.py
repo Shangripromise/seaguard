42589b6583de26ebf52e1ba75e2f835f7cc1e415
Adrian Doehring <your@email.com>
2026-03-16 20:38:45 +0000

    feat(styling): add CSS styling across all templates

--- seaguard/urls.py
+++ seaguard/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib import admin
-from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('vessels/', include('vessels.urls')),
     path('emergencies/', include('emergencies.urls')),
+    path('', RedirectView.as_view(url='/accounts/login/')),
 ]
